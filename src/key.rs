7fa1dd1b222dbee9a2e54265f1d2dfbda206a323
kotoff-studio <76766132+kotoff-studio@users.noreply.github.com>
2023-03-10 23:14:42 +0300

    Update key.rs

--- src/key.rs
+++ src/key.rs
@@ -3,5 +3,7 @@ use std::vec;
 
 fn main() {
     let keys = vec!["spec", "incr", "dpps", "fpde", "absp", "ufst", "satt", "seqr"];
+    let av_keys = vec!["T*9[m/,$?VT7cZzdcPj", "DqhC~JKr/YDTDV8Ia'_", "tp,NiY62VAA+/*H.m9:", "85E|McdURMKw(zZe5TC"];
+    println!("{:?}", keys.choose(&mut rand::thread_rng()));
     println!("{:?}", keys.choose(&mut rand::thread_rng()));
 }
